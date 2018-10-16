5611949879c7cd5e4e75365ba64c5217d4c2f1e1
edumartinez16 <emartinez16@ucol.mx>
2018-10-16 05:38:55 -0500

    Adding relations between tables
    This is necessary in order to have access and create the realtions between tables.
    
    For this, I added one line of code in every file to indicate the type of relation in each table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_15_231337) do
+ActiveRecord::Schema.define(version: 2018_10_16_102447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "status"
+    t.string "description"
+    t.date "limit"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
