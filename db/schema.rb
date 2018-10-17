fdacc9b89c4e2889c133719d4b6abcc3c1a3a82a
edumartinez16 <emartinez16@ucol.mx>
2018-10-17 12:20:20 -0500

    Added new field to the table users
    This was necessary to store the picture of the user.

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2018_10_16_163209) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "picture", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
