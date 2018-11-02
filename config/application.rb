ad5eba839e3f491290e505bab71afa882479703b
edumartinez16 <emartinez16@ucol.mx>
2018-11-01 22:30:05 -0600

    Installed and configured Rspec
    Installed the Rspec Gem along with Capibara and made the necessary configuration

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,16 @@ module Todo
     config.load_defaults 5.2
     config.assets.initialize_on_precompile = false
     config.active_job.queue_adapter = :sidekiq
-
+    config.generators do |g|
+      g.test_framework :rspec,
+        :fixtures => true,
+        :view_specs => false,
+        :helper_specs => false,
+        :routing_specs => false,
+        :controller_specs => true,
+        :request_specs => true
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
